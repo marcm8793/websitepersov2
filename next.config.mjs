5ebd184edbef3aa0081b321a4c3e234755689ad8
Marc Mansour <marcm9387@gmail.com>
2025-12-13 08:53:28 +0100

    chore: update dependencies and configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import rehypePrettyCode from "rehype-pretty-code";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // TurboPack
+  turbopack: {},
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
